b11b24b5f5b5c3bbd8519096b9d9a1c0cffb8d10
Manisankar Chakrabarty <mscabc222@outlook.com>
2025-04-06 13:41:38 +0530

    Update dependencies and create production build

---
+++ build/precache-manifest.c34f0bbfdfc8b268adc33a883068abd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f60facc433c2c1d44a27971aba95ce3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57abcb39de87c5281ef9",
+    "url": "/static/css/main.94eab1e8.chunk.css"
+  },
+  {
+    "revision": "46630ff45c6194f6e38a",
+    "url": "/static/js/2.1857979e.chunk.js"
+  },
+  {
+    "revision": "80c22b709854abab6800481175e1e427",
+    "url": "/static/js/2.1857979e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57abcb39de87c5281ef9",
+    "url": "/static/js/main.746b27b5.chunk.js"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  }
+]);
