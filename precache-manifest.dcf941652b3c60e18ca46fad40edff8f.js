6ce9c01b7bc5256f31d5648801ac5efbfc2915a7
Manisankar Chakrabarty <mscabc222@outlook.com>
2025-04-06 13:46:27 +0530

    Updates

---
+++ precache-manifest.dcf941652b3c60e18ca46fad40edff8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab6682af345db883ef1e3677494481a7",
+    "url": "/Media-website/index.html"
+  },
+  {
+    "revision": "57abcb39de87c5281ef9",
+    "url": "/Media-website/static/css/main.94eab1e8.chunk.css"
+  },
+  {
+    "revision": "46630ff45c6194f6e38a",
+    "url": "/Media-website/static/js/2.1857979e.chunk.js"
+  },
+  {
+    "revision": "80c22b709854abab6800481175e1e427",
+    "url": "/Media-website/static/js/2.1857979e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57abcb39de87c5281ef9",
+    "url": "/Media-website/static/js/main.746b27b5.chunk.js"
+  },
+  {
+    "revision": "4e0ca38d861bcfeefe74",
+    "url": "/Media-website/static/js/runtime-main.5d51d8b2.js"
+  }
+]);
